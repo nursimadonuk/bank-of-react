eb6ba53f898e69e6b5654567a318246760e6b112
Nursima Donuk <nursimadonuk@gmail.com>
2020-01-10 20:24:42 -0500

    linked home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Components/Home';
+import UserProfile from './Components/UserProfile';
 
-import React, { Component } from 'react'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state ={
-      accountBalance: 14568.27
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'bob_loblaw',
+        memberSince: '08/23/99'
+      }
     }
   }
   render() {
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>
+    );
     return (
       <div>
         <Router>
           <Switch>
             <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
           </Switch>
         </Router>
       </div>
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+export default class UserProfile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>User Profile</h1>
+                <div>Username: {this.props.userName}</div>
+                <div>Member Since: {this.props.memberSince}</div>
+                <Link to="/">Home Page</Link>
+            </div>
+        )
+    }
+}
